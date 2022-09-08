db2ad3145cf66b455d9e35cec0d2b8545fbaef5f
zakaufman <zakakaufman@gmail.com>
2022-09-07 20:57:03 -0500

    make into PWA

---
+++ workbox-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	globDirectory: ".",
+	globPatterns: [],
+	globIgnores: ["typings/**"],
+	swDest: "sw.js",
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	runtimeCaching: [
+		{
+			urlPattern: /libs/,
+			handler: "CacheFirst",
+			options: {
+				cacheName: "libs",
+			},
+		},
+		{
+			urlPattern: /\.(?:html|css|js|json|png|wasm)$/,
+			handler: "StaleWhileRevalidate",
+			options: {
+				cacheName: "assets",
+			},
+		},
+	],
+};
